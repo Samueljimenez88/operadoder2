e7e1d4ef7ae3db59e8abeb4b7e898388ac55ff63
Samueljimenez88 <samueljimenezrios67@gmail.com>
2025-08-12 23:30:48 -0500

    Update Main.java

--- src/main/java/com/mycompany/mavenproject3/Main.java
+++ src/main/java/com/mycompany/mavenproject3/Main.java
@@ -11,42 +11,38 @@ import java.util.Scanner;
  * @author mondongo
  */
 public class Main{
-     public static void main(String[] args) {
-        Scanner lector= new Scanner(System.in);
-
-        System.out.print("Digite la nota 1: ");
-        double nota1 = lector.nextDouble();
-
-        System.out.print("Digite la nota 2: ");
-        double nota2 = lector.nextDouble();
-
-        System.out.print("Digite la nota 3: ");
-        double nota3 = lector.nextDouble();
-
-        double p1 = nota1 * 0.30;
-        double p2 = nota2 * 0.30;
-        double p3 = nota3 * 0.40;
-
-        double nf = p1 + p2 + p3;
-         int notafinal;  
-         
-        if (nf >= 3.0) {
-            System.out.println("Ganó con nota: " + nf);
-        } else {
-            System.out.println("Perdió con nota: " + nf);
-        }
-
-        if (nf >= 1.0 && nf <= 2.9) {
-            System.out.println("Desempeño: Deficiente");
-        } else if (nf >= 3.0 && nf <= 3.4) {
-            System.out.println("Desempeño: Insuficiente");
-        } else if (nf >= 3.5 && nf <= 4.4) {
-            System.out.println("Desempeño: Aprobado");
-        } else if (nf >= 4.5 && nf <= 5.0) {
-            System.out.println("Desempeño: Excelente");
-        } else {
-            System.out.println("Nota fuera de rango");
+   public static void main(String[] args) {
+        try (Scanner lector = new Scanner(System.in)) {
+            System.out.print("Ingrese la nota 1: ");
+            double nota1 = lector.nextDouble();
+            
+            System.out.print("Ingrese la nota 2: ");
+            double nota2 = lector.nextDouble();
+            
+            System.out.print("Ingrese la nota 3: ");
+            double nota3 = lector.nextDouble();
+            
+            double p1 = nota1 * 0.30;
+            double p2 = nota2 * 0.30;
+            double p3 = nota3 * 0.40;
+            
+            double nf = p1 + p2 + p3;
+            
+            System.out.println("\nNota final: " + nf);
+            
+            if (nf >= 1.0 && nf <= 1.9) {
+                System.out.println("Desempeño: Deficiente");
+            } else if (nf >= 2.0 && nf <= 2.9) {
+                System.out.println("Desempeño: Insuficiente");
+            } else if (nf >= 3.0 && nf <= 3.9) {
+                System.out.println("Desempeño: Aceptable");
+            } else if (nf >= 4.0 && nf <= 5.0) {
+                System.out.println("Desempeño: Destacado");
+            } else {
+                System.out.println("Nota fuera de rango.");
+            }
         }
     }
 }
+
 
